df7b22eed36f05657cac82ace0a4f9fded59c57a
Siddharth <officialsiddharthbisht@gmail.com>
2022-07-25 14:09:53 +0530

    initializing passport js

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
+const passport = require('passport');
 
+const initalizePassport = require('./passport-config');
+initalizePassport(passport);
+
+const users = [];
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 
@@ -18,8 +24,20 @@ app.get('/register',(req ,res)=>{
     res.render(`register.ejs`);
 })
 
-app.post('/register',(req , res)=>{
-
+app.post('/register',async (req , res)=>{
+    try{
+        const hashedPassword = await bcrypt.hashSync(req.body.password,10);
+        users.push({
+            id : Date.now().toString(),
+            name : req.body.name,
+            email : req.body.email,
+            password : hashedPassword
+        })
+        res.redirect('/login');
+    }catch(err){
+        res.redirect('/register');
+    }
+    console.log(users);
 })
 
 app.listen(3000,()=>{
